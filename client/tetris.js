bf27381556b23e99b202490da126e11c7f426566
Eyskha <66302196+Eyskha@users.noreply.github.com>
2021-03-28 20:01:44 +0200

    multi : vision des 2 + envoi de lignes

--- client/tetris.js
+++ client/tetris.js
@@ -1,9 +1,11 @@
-const canvas = document.getElementById('tetris');
+const canvas = document.getElementById('canvasjoueur1');
 const context = canvas.getContext('2d');
-
-//TEST
+const canvasjoueur2 = document.getElementById('canvasjoueur2');
+const contextjoueur2 = canvasjoueur2.getContext('2d');
+var socket = io();
 
 context.scale(20, 20);
+contextjoueur2.scale(20, 20);
 
 function arenaSweep() {
     let rowCount = 1;
@@ -18,36 +20,48 @@ function arenaSweep() {
         arena.unshift(row);
         ++y;
 
-        player.score += rowCount * 10;
+        joueur.score += rowCount * 10;
         rowCount *= 2;
     }
+	
+	// Envoi des lignes à l'adversaire
+	if(rowCount != 1){
+		socket.emit('full rows', {
+			nbrows: rowCount/2
+		});
+	}
 }
 
-function collide(arena, player) {
-    const m = player.matrix;
-    const o = player.pos;
-    for (let y = 0; y < m.length; ++y) {
-        for (let x = 0; x < m[y].length; ++x) {
-            if (m[y][x] !== 0 &&
-               (arena[y + o.y] &&
-                arena[y + o.y][x + o.x]) !== 0) {
-                return true;
+function gestion_collision(arena, joueur) {  //on regarde si il y a collision entre la pièce en train d'être jouée et une pièce déjà présente dans l'arène
+    for (let y = 0; y < joueur.matrix.length; ++y) {
+        for (let x = 0; x < joueur.matrix[y].length; ++x) {
+            if (joueur.matrix[y][x] !== 0 &&
+               (arena[y + joueur.pos.y] &&
+                arena[y + joueur.pos.y][x + joueur.pos.x]) !== 0) {
+                return true;  //il y a collision , le programme se termine et retourne true
             }
         }
     }
-    return false;
+    return false;  //pas de collision
 }
 
-function createMatrix(w, h) {
+function creer_Matrix(w, h) {  //pour créer l'arène initiale
     const matrix = new Array(h);
-    for (var i=0; i<h; i++){
+    for (var i=0;i<h;i++){
         matrix[i] = new Array(w).fill(0);
     }
     return matrix;
 }
 
-function NewPiece(type)
-{
+function nombre_entier_aleatoire(max) {
+    var a=0;
+    while(a==0){
+        a=Math.floor(Math.random() * Math.floor(max));
+    }
+    return a;
+}
+
+function NewPiece(type){
     if (type === 'I') {
         return [
             [0, 1, 0, 0],
@@ -74,7 +88,7 @@ function NewPiece(type)
         ];
     } else if (type === 'Z') {
         return [
-            [5,5, 0],
+            [5, 5, 0],
             [0, 5, 5],
             [0, 0, 0],
         ];
@@ -93,38 +107,36 @@ function NewPiece(type)
     }
 }
 
-function drawMatrix(matrix, offset) {
-    matrix.forEach((row, y) => {
-        row.forEach((value, x) => {
-            if (value !== 0) {
-                context.fillStyle = couleurs_pieces[value];
-                context.fillRect(x + offset.x,
-                                 y + offset.y,
-                                 1, 1);
+function drawMatrix(matrix, offset, inputcontext) {  //on dessine les pièces
+    for (y=0;y<matrix.length;y++){
+        for (x=0;x<matrix[y].length;x++){
+            if (matrix[y][x] !==0){
+                inputcontext.fillStyle = couleurs_pieces[matrix[y][x]];
+                inputcontext.fillRect(x + offset.x,y + offset.y,1, 1);
             }
-        });
-    });
+        }
+    }
 }
 
-function draw() {
-    context.fillStyle = '#000';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    drawMatrix(arena, {x: 0, y: 0});
-    drawMatrix(player.matrix, player.pos);
+function drawArena(inputcontext, inputcanvas, inputarena, inputjoueur) {
+    inputcontext.fillStyle = 'black';   //couleur du background de l'arène
+    inputcontext.fillRect(0, 0, inputcanvas.width, inputcanvas.height);
+	
+    drawMatrix(inputarena, {x: 0, y: 0}, inputcontext);  //on dessine les pièces déjà présentes
+    drawMatrix(inputjoueur.matrix, inputjoueur.pos, inputcontext);  //on dessine la pièce qui est en train de tomber
 }
 
-function merge(arena, player) {
-    player.matrix.forEach((row, y) => {
-        row.forEach((value, x) => {
-            if (value !== 0) {
-                arena[y + player.pos.y][x + player.pos.x] = value;
+function ajout_piece_joueur(arena, joueur) {  //on ajoute la matrice du joueur à l'arène, cette pièce est placée définitivement (il y avait collision)
+    for (y=0;y<joueur.matrix.length;y++){
+        for (x=0;x<joueur.matrix[y].length;x++){
+            if (joueur.matrix[y][x] !== 0) {
+                arena[y + joueur.pos.y][x + joueur.pos.x] = joueur.matrix[y][x];
             }
-        });
-    });
+        };
+    };
 }
 
-function rotate(matrix, dir) {
+function rotation(matrix, dir) {
     for (let y = 0; y < matrix.length; ++y) {
         for (let x = 0; x < y; ++x) {
             [
@@ -144,48 +156,59 @@ function rotate(matrix, dir) {
     }
 }
 
+var nbappels = 0;
 function playerDrop() {
-    player.pos.y++;
-    if (collide(arena, player)) {
-        player.pos.y--;
-        merge(arena, player);
+	nbappels += 1; // nombre d'appels à la fonction playerDrop (pour 
+    joueur.pos.y++;
+    if (gestion_collision(arena, joueur)) {
+        joueur.pos.y--;
+        ajout_piece_joueur(arena, joueur);
         playerReset();
         arenaSweep();
         updateScore();
+		
+		nbRowsReceived = Math.ceil(nbRowsReceived/nbappels);
+		addRowsArena(nbRowsReceived);
+		nbRowsReceived = 0;
     }
     dropCounter = 0;
+	
+	// Contrôle si adversaire a envoyé des lignes
+	socket.on('gift from player 2', data => {
+		nbRowsReceived += data.nbrows;
+	});
 }
 
 function playerMove(offset) {
-    player.pos.x += offset;
-    if (collide(arena, player)) {
-        player.pos.x -= offset;
+    joueur.pos.x += offset;
+    if (gestion_collision(arena, joueur)) {
+        joueur.pos.x -= offset;
     }
 }
 
 function playerReset() {
     const pieces = 'TJLOSZI';
-    player.matrix = NewPiece(pieces[pieces.length * Math.random() | 0]);
-    player.pos.y = 0;
-    player.pos.x = (arena[0].length / 2 | 0) -
-                   (player.matrix[0].length / 2 | 0);
-    if (collide(arena, player)) {
+    joueur.matrix = NewPiece(pieces[pieces.length * Math.random() | 0]);
+    joueur.pos.y = 0;
+    joueur.pos.x = (arena[0].length / 2 | 0) -
+                   (joueur.matrix[0].length / 2 | 0);
+    if (gestion_collision(arena, joueur)) {
         arena.forEach(row => row.fill(0));
-        player.score = 0;
+        joueur.score = 0;
         updateScore();
     }
 }
 
-function playerRotate(dir) {
-    const pos = player.pos.x;
+function rotation_piece(dir) {
+    const pos = joueur.pos.x;
     let offset = 1;
-    rotate(player.matrix, dir);
-    while (collide(arena, player)) {
-        player.pos.x += offset;
+    rotation(joueur.matrix, dir);
+    while (gestion_collision(arena, joueur)) {
+        joueur.pos.x += offset;
         offset = -(offset + (offset > 0 ? 1 : -1));
-        if (offset > player.matrix[0].length) {
-            rotate(player.matrix, -dir);
-            player.pos.x = pos;
+        if (offset > joueur.matrix[0].length) {
+            rotation(joueur.matrix, -dir);
+            joueur.pos.x = pos;
             return;
         }
     }
@@ -201,16 +224,44 @@ function update(time = 0) {
     dropCounter += deltaTime;
     if (dropCounter > dropInterval) {
         playerDrop();
+		socket.on('arena player 2', data => {
+			arenajoueur2 = data.player2arena;
+			joueur2 = data.player2joueur;
+			drawArena(contextjoueur2, canvasjoueur2, arenajoueur2,joueur2);
+		});
     }
-
+	
     lastTime = time;
+	
+	socket.emit('arena update', {
+		arena: arena,
+		joueur: joueur
+	});
 
-    draw();
+    drawArena(context, canvas, arena, joueur);
     requestAnimationFrame(update);
+	
+}
+
+function addRowsArena(nbrows){
+	if(nbrows > 0){
+		// Si 1ere ligne est non vide alors defaite
+		if(arena[0].reduce((a,b) => a+b) != 0){
+			// Game over
+		}
+		else {
+			var aleat = nombre_entier_aleatoire(12);
+			for(var i=0; i<nbrows; i++){
+				const row = arena.splice(0, 1)[0].fill(8)
+				row[aleat] = 0;
+				arena.push(row);
+			}
+		}		
+	}
 }
 
 function updateScore() {
-    document.getElementById('score').innerText = player.score;
+    document.getElementById('score').innerText = joueur.score;
 }
 
 document.addEventListener('keydown', event => {
@@ -220,31 +271,45 @@ document.addEventListener('keydown', event => {
         playerMove(1);
     } else if (event.keyCode === 40) {
         playerDrop();
-    } else if (event.keyCode === 81) {
-        playerRotate(-1);
-    } else if (event.keyCode === 87) {
-        playerRotate(1);
+    } else if (event.keyCode === 65) {
+        rotation_piece(-1);
+    } else if (event.keyCode === 90) {
+        rotation_piece(1);
     }
 });
 
 const couleurs_pieces = [
     null,
-    '#FF0D72',
-    '#0DC2FF',
-    '#0DFF72',
-    '#F538FF',
-    '#FF8E0D',
-    '#FFE138',
-    '#3877FF',
+    '#ee31c5',
+    '#ff3f34',
+    '#ffd32a',
+    '#ffa000',
+    '#0fbcf9',
+    '#05c46b',
+    '#3c40c6',
+	'#ffffff'
 ];
 
-const arena = createMatrix(12, 20);
-
-const player = {
+const arena = creer_Matrix(12, 20);
+const joueur = {
     pos: {x: 0, y: 0},
     matrix: null,
     score: 0,
 };
+
+var arenajoueur2 = creer_Matrix(12, 20);
+var joueur2 = {
+    pos: {x: 0, y: 0},
+    matrix: null,
+    score: 0,
+};
+
+var nbRowsReceived = 0;
+
+socket.on('user connected', data => {
+	var idjoueur = data.userID;
+	console.log(idjoueur);
+});
 
 playerReset();
 updateScore();
--- serveur.js
+++ serveur.js
@@ -3,13 +3,37 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+var connectedPlayers = [];
+
 app.use(express.static(__dirname+'/client'));
 
 io.on('connection', (socket) => {
   console.log('user connected');
-  socket.emit('test',{'data': 'this is my data'});
+  connectedPlayers.push(socket.id);
+  console.log(connectedPlayers);
+  
+  socket.emit("user connected", {
+    userID: socket.id
+  });
+  
+  socket.on('arena update', data => {
+	socket.broadcast.emit('arena player 2', {
+		player2arena: data.arena,
+		player2joueur: data.joueur,
+	});
+  });
+  
+  socket.on('full rows', data => {
+	console.log(data);
+	socket.broadcast.emit('gift from player 2', {
+		nbrows: data.nbrows
+	});
+  });
+  
   socket.on('disconnect', () => {
-	  console.log('user disconnected');
+	console.log('user disconnected');
+	connectedPlayers.splice(connectedPlayers.indexOf(socket.id),1);
+	console.log(connectedPlayers);
   });
 });
 
